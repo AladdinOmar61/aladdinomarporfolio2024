69f5f17632b1b7510573b1223a33db3e2a429ce9
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-04-09 03:48:22 -0400

    started adding grid. May take short hiatus to work on individual projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { Box, Grid } from 'grommet'
+import { Box, Heading, Image } from 'grommet'
+import joujou from "./assets/joujou.jpg"
 
 function App() {
 
-  //TODO: continue looking at storybook documentation for grommet
+  const gridSize = {height: 200, width: 300}
+
+  //TODO: make a grommet theme of red/white
+  
+  //TODO: continue working on grid, need at least two more projects 
+  //for portfolio.
   return (
-    <Box>
-      <Grid></Grid>
+    <>
+    <Heading level={1} style={{textAlign: 'center'}}>Aladdin Omar Portfolio</Heading>
+    <Box direction='row' justify='center' alignContent={'around'} align={'center'} flex={true} gap={"small"}>
+      <Box style={{border: 'solid black 1px'}} background="brand" gridArea="main">
+        <Image src={joujou} style={gridSize}/>
+      </Box>
+
+      <Box style={gridSize} background="light-5" gridArea="main">
+        Sidebar
+      </Box>
+
+      <Box style={gridSize} background="light-2" gridArea="main">
+        Main
+      </Box>
+
+      <Box style={gridSize} background="dark-2" gridArea="main">
+        Footer
+      </Box>
     </Box>
+    </>
   )
 }
 
