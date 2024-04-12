adf8ee5e9b17031f583d5901bcb2d57769fa8f15
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-04-12 01:43:56 -0400

    created projects section
    using an array of objects up top for neater and more scalable code for when I add each individual project. This will make it nice and organized and easy for me to add new ones down the line

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { Box, Heading, Image } from 'grommet'
+import { Box, Grommet, Heading, Image, Card, CardHeader, CardBody, CardFooter, Button } from 'grommet'
 import joujou from "./assets/joujou.jpg"
+import { Domain, Github } from 'grommet-icons';
 
 function App() {
 
-  const gridSize = {height: 200, width: 300}
+  const projArr = [{ name: 'project1', image: joujou }, { name: 'project2', image: joujou }, { name: 'project3', image: joujou }, { name: 'project4', image: joujou }];
 
-  //TODO: make a grommet theme of red/white
-  
-  //TODO: continue working on grid, need at least two more projects 
-  //for portfolio.
+  const theme = {
+    global: {
+      colors: {
+        brand: 'red',
+        focus: 'white',
+      },
+      font: {
+        family: 'Rajdhani',
+        size: '18px',
+        height: '20px',
+      },
+      background: {
+        image: joujou,
+        size: 'cover',
+        position: 'center',
+      },
+    }
+  }
+
   return (
-    <>
-    <Heading level={1} style={{textAlign: 'center'}}>Aladdin Omar Portfolio</Heading>
-    <Box direction='row' justify='center' alignContent={'around'} align={'center'} flex={true} gap={"small"}>
-      <Box style={{border: 'solid black 1px'}} background="brand" gridArea="main">
-        <Image src={joujou} style={gridSize}/>
+    <Grommet theme={theme} full>
+      <Box>
+        <Heading level={1} textAlign="center" alignSelf="center">Aladdin Omar Portfolio</Heading>
       </Box>
-
-      <Box style={gridSize} background="light-5" gridArea="main">
-        Sidebar
+      <Box direction='row' justify='center' alignContent={'around'} align={'center'} flex={true} gap={"large"}>
+        {projArr.map((proj, i) => {
+          return (
+            <Card key={i} style={{ padding: 10 }}>
+              <CardHeader alignSelf="center">
+                {proj.name}
+              </CardHeader>
+              <CardBody>
+                <Image src={proj.image} fit='contain' style={{ height: 200, width: 300 }} />
+              </CardBody>
+              <CardFooter pad={{ horizontal: "small" }} style={{ padding: 5 }}>
+                <Box width='full' direction='row' gap="large" justify='center' alignContent='center'>
+                  <Button style={{ border: "solid red 2px", borderRadius: 50, padding: 3, transition: 'background-color 0.3s ease' }} hoverIndicator={{ background: 'red', elevation: "medium" }}
+                    icon={
+                      <Box hoverIndicator='white' round='full'>
+                        <Github style={{ width: 70, height: 30, transition: 'color 0.3s ease' }} color='black' />
+                      </Box>
+                    }
+                  />
+                  <Button style={{ border: "solid red 2px", borderRadius: 50, padding: 3 }} icon={<Domain style={{ width: 70, height: 30 }} color='black' />} />
+                </Box>
+              </CardFooter>
+            </Card>
+          )
+        })}
       </Box>
-
-      <Box style={gridSize} background="light-2" gridArea="main">
-        Main
-      </Box>
-
-      <Box style={gridSize} background="dark-2" gridArea="main">
-        Footer
-      </Box>
-    </Box>
-    </>
+    </Grommet>
   )
 }
 
