5c13d05bb5310137203bae7cc66b035d8cca81ec
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-04-17 17:41:32 -0400

    attempting to replace cards with boxes for responsiveness

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,11 +1,11 @@
-import { ResponsiveContext } from "grommet";
+import { ResponsiveContext, Text } from "grommet";
 import React from "react";
 import {
   Image,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
+  // Card,
+  // CardHeader,
+  // CardBody,
+  // CardFooter,
   Button,
   Box,
 } from "grommet";
@@ -22,7 +22,7 @@ const projArr = [
 
 function Projects() {
   const size = React.useContext(ResponsiveContext);
-    console.log(size);
+  console.log(size);
 
   return (
     <Box
@@ -48,59 +48,87 @@ function Projects() {
         }
 
         return (
-          <Card key={i} background="white" width={cardSize} height={cardSize} fill={"horizontal"}>
-            <CardHeader alignSelf="center">{proj.name}</CardHeader>
-            <CardBody>
-              <Image
-                src={proj.image}
-                fit="contain"
-              />
-            </CardBody>
-            <CardFooter pad={{ horizontal: "small" }} style={{ padding: 5 }}>
-              <Box
-                width="full"
-                direction="row"
-                gap="large"
-                justify="center"
-                alignContent="center"
-              >
+          // <Card key={i} background="white" width={cardSize} height={cardSize} fill={"horizontal"}>
+          //   <CardHeader alignSelf="center">{proj.name}</CardHeader>
+          //   <CardBody>
+          //     <Image
+          //       src={proj.image}
+          //       fit="contain"
+          //     />
+          //   </CardBody>
+          //   <CardFooter pad={{ horizontal: "small" }} style={{ padding: 5 }}>
+          //     <Box
+          //       width="full"
+          //       direction="row"
+          //       gap="large"
+          //       justify="center"
+          //       alignContent="center"
+          //     >
+          //       <Button
+          //         style={{
+          //           border: "solid red 2px",
+          //           borderRadius: 50,
+          //           padding: 3,
+          //           transition: "background-color 0.3s ease",
+          //         }}
+          //         hoverIndicator={{
+          //           background: "red",
+          //           elevation: "medium",
+          //         }}
+          //         icon={
+          //           <Box hoverIndicator="white" round="full">
+          //             <Github
+          //               style={{
+          //                 width: 80,
+          //                 height: 35,
+          //                 transition: "color 0.3s ease",
+          //               }}
+          //               color="black"
+          //             />
+          //           </Box>
+          //         }
+          //       />
+          //       <Button
+          //         style={{
+          //           border: "solid red 2px",
+          //           borderRadius: 50,
+          //           padding: 3,
+          //         }}
+          //         icon={
+          //           <Domain style={{ width: 80, height: 35 }} color="black" />
+          //         }
+          //       />
+          //     </Box>
+          //   </CardFooter>
+          // </Card>
+          <Box
+            key={i}
+            direction="column"
+            width={cardSize}
+            height={cardSize}
+            border={{ color: "black", size: "small" }}
+            round={true}
+            background={"light-1"}
+          >
+            <Text textAlign="center">{proj.name}</Text>
+            <Box style={{ height: "100%" }}>
+              <Image src={proj.image} fit="contain" />
+            </Box>
+            <Box style={{ height: "50%" }}>
+              <Box direction="row" justify="evenly" gap="small" margin={{top: "medium"}}>
                 <Button
-                  style={{
-                    border: "solid red 2px",
-                    borderRadius: 50,
-                    padding: 3,
-                    transition: "background-color 0.3s ease",
-                  }}
-                  hoverIndicator={{
-                    background: "red",
-                    elevation: "medium",
-                  }}
-                  icon={
-                    <Box hoverIndicator="white" round="full">
-                      <Github
-                        style={{
-                          width: 80,
-                          height: 35,
-                          transition: "color 0.3s ease",
-                        }}
-                        color="black"
-                      />
-                    </Box>
-                  }
+                  icon={<Github />}
+                  hoverIndicator={{ color: "red" }}
+                  style={{ borderRadius: 50 }}
                 />
                 <Button
-                  style={{
-                    border: "solid red 2px",
-                    borderRadius: 50,
-                    padding: 3,
-                  }}
-                  icon={
-                    <Domain style={{ width: 80, height: 35 }} color="black" />
-                  }
+                  icon={<Domain />}
+                  hoverIndicator={{ color: "red" }}
+                  style={{ borderRadius: 50 }}
                 />
               </Box>
-            </CardFooter>
-          </Card>
+            </Box>
+          </Box>
         );
       })}
     </Box>
